7b2ba5b917f859c9b68ea71a19efd7a19d4edb56
Muhammad Shayan Anwar <155350566+Shayan-codingways@users.noreply.github.com>
2024-04-13 08:32:31 +0500

    Add files via upload

---
+++ arrays.ts
@@ -0,0 +1,10 @@
+let fruitchaat = ["Banana","Apple","Melon","Mango"];
+console.log(fruitchaat)
+
+fruitchaat.push("Cheeeko")
+fruitchaat.pop()
+fruitchaat.pop()
+console.log(fruitchaat)
+
+let update_fruitchaat=fruitchaat.concat(["ASDF","sdfc"])
+console.log(update_fruitchaat)
---
+++ index.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+//empty array for tasks
+let todos=[];
+
+let condition=true;
+while(condition){
+
+    let addTAsk= await inquirer.prompt(
+        [
+
+        //object 1
+            {
+                name:'todo',
+                type:"input",
+                message:"What you want to to add in to your Todos?"
+            },
+        
+        //object 2
+            {
+                name:"addMore",
+                type:"confirm",
+                message:"Do you want to add more?",
+                default: "false"
+            }
+    
+        ]
+    );
+
+    //addition in todas array
+    todos.push(addTAsk.todo);
+    condition=addTAsk.addMore;
+    console.log(todos)
+
+
+}
